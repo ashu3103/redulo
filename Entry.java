71229f94c09a49716f61ef8f25dbc5aa61efc592
ashu3103 <ash2003sharma@gmail.com>
2026-08-30 13:36:33 +0530

    make reading the arguments safe

--- Entry.java
+++ Entry.java
@@ -3,6 +3,12 @@ import soot.options.Options;
 
 public class Entry {
     public static void main(String[] args) {
+        // Check if the required number of arguments provided
+        if (args.length < 1) {
+            System.out.println("Usage: java [options] Entry [arg]");
+            System.exit(1);
+        }
+        
         String classPath = "./testcases/" + args[0];
     
         // Set up arguments for Soot
@@ -11,19 +17,16 @@ public class Entry {
             "-pp",  // sets the class path for Soot
             "-f", "J",
             "-t", "1",
-            "-main-class", "Test", // specify the main class
+            "-main-class", args[0], // specify the main class
             "-process-dir", classPath
         };
 
         // create transformer for analysis
         AnalysisTransformer analysisTransformer = new AnalysisTransformer();
-
         // Add transformer to the appropriate pack in PackManager; PackManager will run all packs when soot.Main.main is called
         PackManager.v().getPack("jtp").add(new Transform("jtp.dfa", analysisTransformer));
-
         // Set Soot options
         Options.v().set_keep_line_number(true);
-
         // Call Soot's main method with arguments
         soot.Main.main(sootArgs);
     }
