086fbd1c77d9dffae454e1e5f7cc586f8cf2dd11
ashu3103 <ash2003sharma@gmail.com>
2026-08-30 13:24:50 +0530

    integrate analysis to transformer

--- AnalysisTransformer.java
+++ AnalysisTransformer.java
@@ -1,18 +1,14 @@
+
 import java.util.*;
 import soot.*;
-import soot.jimple.AnyNewExpr;
-import soot.jimple.Ref;
-import soot.jimple.internal.JAssignStmt;
-import soot.jimple.internal.JNewExpr;
-import soot.toolkits.graph.*;
-import soot.toolkits.scalar.BackwardFlowAnalysis;
-import soot.toolkits.scalar.FlowSet;
+import soot.toolkits.graph.UnitGraph;
+import soot.toolkits.graph.BriefUnitGraph;
 
 public class AnalysisTransformer extends BodyTransformer {
     @Override
     protected void internalTransform(Body body, String phaseName, Map<String, String> options) {
-
-
+        UnitGraph ug = new BriefUnitGraph(body);
+        RedundantFieldEliminationAnalysis analysis = new RedundantFieldEliminationAnalysis(ug);
     }
 }
 
