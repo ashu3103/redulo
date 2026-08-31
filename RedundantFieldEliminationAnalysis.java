94e6100d061d9cec7c2947a93acb65869ef75b79
ashu3103 <ash2003sharma@gmail.com>
2026-08-31 09:00:00 +0530

    initialization of universe

--- RedundantFieldEliminationAnalysis.java
+++ RedundantFieldEliminationAnalysis.java
@@ -4,13 +4,11 @@ import soot.*;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Iterator;
 import java.util.List;
 
-import soot.Value;
-import soot.ValueBox;
-import soot.Unit;
 import soot.toolkits.graph.DirectedGraph;
 import soot.toolkits.graph.UnitGraph;
 import soot.toolkits.scalar.ArrayFlowUniverse;
@@ -20,55 +18,93 @@ import soot.toolkits.scalar.FlowSet;
 import soot.toolkits.scalar.FlowUniverse;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 import soot.jimple.FieldRef;
+import soot.jimple.InstanceFieldRef;
+import soot.SootField;
 
-public class RedundantFieldEliminationAnalysis extends ForwardFlowAnalysis<Unit, FlowSet<Value>> {
-    protected final Map<Unit, BoundedFlowSet<Value>> unitToKillSet;
-    protected final Map<Unit, BoundedFlowSet<Value>> unitToGenerateSet;
-    protected final FlowSet<Value> emptySet;
+public class RedundantFieldEliminationAnalysis extends ForwardFlowAnalysis<Unit, FlowSet<RedundantFieldEliminationAnalysis.FieldRefKey>> {
+    /** Identifies a specific "base.field" reference for caching purposes. */
+    public static final class FieldRefKey {
+        final Value base;
+        final SootField field;
+ 
+        FieldRefKey(Value base, SootField field) {
+            this.base = base;
+            this.field = field;
+        }
+ 
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof FieldRefKey)) {
+                return false;
+            }
+            FieldRefKey other = (FieldRefKey) o;
+            return base.equivTo(other.base) && field.equals(other.field);
+        }
+
+        @Override
+        public int hashCode() {
+            // We can't derive a hash consistent with equivTo() for an
+            // arbitrary Value, so we only hash on the field. This is still
+            // correct (equal objects hash equal), just coarser-grained.
+            return 31 * field.hashCode();
+        }
+ 
+        @Override
+        public String toString() {
+            return base + "." + field.getSignature();
+        }
+
+    }
+
+    protected final Map<Unit, FlowSet<FieldRefKey>> unitToKillSet;
+    protected final Map<Unit, FlowSet<FieldRefKey>> unitToGenerateSet;
+    protected final FlowSet<FieldRefKey> emptySet;
+    private final List<FieldRefKey> universe;
+
 
     public RedundantFieldEliminationAnalysis(DirectedGraph<Unit> dg) {
         super(dg);
-        HashSet<Value> fieldRefs = new HashSet<>();
+        // Universe of all field references that appear anywhere in the body.
+        LinkedHashSet<FieldRefKey> fieldRefs = new LinkedHashSet<>();
         /* we need a universe of all field references */
         final UnitGraph g = (UnitGraph) dg;
         for (Unit u : g.getBody().getUnits()) {
-            // print all values of defs
-            List<ValueBox> defs = u.getDefBoxes();
-            for (ValueBox d : defs) {
-                Value v = d.getValue();
-                if (v instanceof FieldRef) {
-                    FieldRef fieldRef = (FieldRef) v;
-                    System.out.println("Unit: " + u);
-                    System.out.println("FieldRef: " + fieldRef);
+            for (ValueBox box : u.getUseAndDefBoxes()) {
+                Value v = box.getValue();
+                if (v instanceof InstanceFieldRef) {
+                    InstanceFieldRef ref = (InstanceFieldRef) v;
+                    fieldRefs.add(new FieldRefKey(ref.getBase(), ref.getField()));
                 }
             }
         }
 
         /* initialize the emptySet */
-        FlowUniverse<Value> fieldRefsUniv = new ArrayFlowUniverse<Value>(fieldRefs.toArray(new Value[fieldRefs.size()]));
-        this.emptySet = new ArrayPackedSet<Value>(fieldRefsUniv);
-        /* create kill set */
-        
-        this.unitToKillSet = new HashMap<Unit, BoundedFlowSet<Value>>(g.size() * 2 + 1, 0.7f);
-        this.unitToGenerateSet = new HashMap<Unit, BoundedFlowSet<Value>>(g.size() * 2 + 1, 0.7f);
+        this.universe = new ArrayList<>(fieldRefs);
+        FlowUniverse<FieldRefKey> fieldRefsUniv =
+                new ArrayFlowUniverse<>(universe.toArray(new FieldRefKey[0]));
+        this.emptySet = new ArrayPackedSet<>(fieldRefsUniv);
+
+        this.unitToKillSet = new HashMap<>(g.size() * 2 + 1, 0.7f);
+        this.unitToGenerateSet = new HashMap<>(g.size() * 2 + 1, 0.7f);
+
         /* create gen set */
         // doAnalysis();
     }
     
     @Override
-    protected FlowSet<Value> newInitialFlow() {
-        BoundedFlowSet<Value> out = (BoundedFlowSet) emptySet.clone();
+    protected FlowSet<FieldRefKey> newInitialFlow() {
+        BoundedFlowSet<FieldRefKey> out = (BoundedFlowSet) emptySet.clone();
         out.complement(out);
         return out;
     }
 
     @Override
-    protected FlowSet<Value> entryInitialFlow() {
+    protected FlowSet<FieldRefKey> entryInitialFlow() {
         return emptySet.clone();
     }
 
     @Override
-    protected void flowThrough(FlowSet<Value> out, Unit u, FlowSet<Value> in) {
+    protected void flowThrough(FlowSet<FieldRefKey> out, Unit u, FlowSet<FieldRefKey> in) {
         // Perform kill
         in.intersection(unitToKillSet.get(u), out);
         // Perform gen
@@ -76,12 +112,12 @@ public class RedundantFieldEliminationAnalysis extends ForwardFlowAnalysis<Unit,
     }
 
     @Override
-    protected void merge(FlowSet<Value> out1, FlowSet<Value> out2, FlowSet<Value> in) {
+    protected void merge(FlowSet<FieldRefKey> out1, FlowSet<FieldRefKey> out2, FlowSet<FieldRefKey> in) {
         out1.intersection(out2, in);
     }
 
     @Override
-    protected void copy(FlowSet<Value> out, FlowSet<Value> in) {
+    protected void copy(FlowSet<FieldRefKey> out, FlowSet<FieldRefKey> in) {
         out.copy(in);
     }
 }
